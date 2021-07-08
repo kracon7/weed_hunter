ce9a597ad28a991308c64b1423c35b73d5096c68
jiacheng <ethancooper007@gmail.com>
2021-07-08 15:50:44 -0500

    add extract bag

--- rr_control_input_manager/scripts/extract_bag.py
+++ rr_control_input_manager/scripts/extract_bag.py
@@ -1,57 +1,57 @@
-#! /home/jc/Envs/py36/bin/python3.6
+#!/usr/bin/env python
 
-name_to_dtypes = {
-    "rgb8":    (np.uint8,  3),
-    "rgba8":   (np.uint8,  4),
-    "rgb16":   (np.uint16, 3),
-    "rgba16":  (np.uint16, 4),
-    "bgr8":    (np.uint8,  3),
-    "bgra8":   (np.uint8,  4),
-    "bgr16":   (np.uint16, 3),
-    "bgra16":  (np.uint16, 4),
-    "mono8":   (np.uint8,  1),
-    "mono16":  (np.uint16, 1),
+# name_to_dtypes = {
+#     "rgb8":    (np.uint8,  3),
+#     "rgba8":   (np.uint8,  4),
+#     "rgb16":   (np.uint16, 3),
+#     "rgba16":  (np.uint16, 4),
+#     "bgr8":    (np.uint8,  3),
+#     "bgra8":   (np.uint8,  4),
+#     "bgr16":   (np.uint16, 3),
+#     "bgra16":  (np.uint16, 4),
+#     "mono8":   (np.uint8,  1),
+#     "mono16":  (np.uint16, 1),
     
-    # for bayer image (based on cv_bridge.cpp)
-    "bayer_rggb8":  (np.uint8,  1),
-    "bayer_bggr8":  (np.uint8,  1),
-    "bayer_gbrg8":  (np.uint8,  1),
-    "bayer_grbg8":  (np.uint8,  1),
-    "bayer_rggb16": (np.uint16, 1),
-    "bayer_bggr16": (np.uint16, 1),
-    "bayer_gbrg16": (np.uint16, 1),
-    "bayer_grbg16": (np.uint16, 1),
+#     # for bayer image (based on cv_bridge.cpp)
+#     "bayer_rggb8":  (np.uint8,  1),
+#     "bayer_bggr8":  (np.uint8,  1),
+#     "bayer_gbrg8":  (np.uint8,  1),
+#     "bayer_grbg8":  (np.uint8,  1),
+#     "bayer_rggb16": (np.uint16, 1),
+#     "bayer_bggr16": (np.uint16, 1),
+#     "bayer_gbrg16": (np.uint16, 1),
+#     "bayer_grbg16": (np.uint16, 1),
 
-    # OpenCV CvMat types
-    "8UC1":    (np.uint8,   1),
-    "8UC2":    (np.uint8,   2),
-    "8UC3":    (np.uint8,   3),
-    "8UC4":    (np.uint8,   4),
-    "8SC1":    (np.int8,    1),
-    "8SC2":    (np.int8,    2),
-    "8SC3":    (np.int8,    3),
-    "8SC4":    (np.int8,    4),
-    "16UC1":   (np.uint16,   1),
-    "16UC2":   (np.uint16,   2),
-    "16UC3":   (np.uint16,   3),
-    "16UC4":   (np.uint16,   4),
-    "16SC1":   (np.int16,  1),
-    "16SC2":   (np.int16,  2),
-    "16SC3":   (np.int16,  3),
-    "16SC4":   (np.int16,  4),
-    "32SC1":   (np.int32,   1),
-    "32SC2":   (np.int32,   2),
-    "32SC3":   (np.int32,   3),
-    "32SC4":   (np.int32,   4),
-    "32FC1":   (np.float32, 1),
-    "32FC2":   (np.float32, 2),
-    "32FC3":   (np.float32, 3),
-    "32FC4":   (np.float32, 4),
-    "64FC1":   (np.float64, 1),
-    "64FC2":   (np.float64, 2),
-    "64FC3":   (np.float64, 3),
-    "64FC4":   (np.float64, 4)
-}
+#     # OpenCV CvMat types
+#     "8UC1":    (np.uint8,   1),
+#     "8UC2":    (np.uint8,   2),
+#     "8UC3":    (np.uint8,   3),
+#     "8UC4":    (np.uint8,   4),
+#     "8SC1":    (np.int8,    1),
+#     "8SC2":    (np.int8,    2),
+#     "8SC3":    (np.int8,    3),
+#     "8SC4":    (np.int8,    4),
+#     "16UC1":   (np.uint16,   1),
+#     "16UC2":   (np.uint16,   2),
+#     "16UC3":   (np.uint16,   3),
+#     "16UC4":   (np.uint16,   4),
+#     "16SC1":   (np.int16,  1),
+#     "16SC2":   (np.int16,  2),
+#     "16SC3":   (np.int16,  3),
+#     "16SC4":   (np.int16,  4),
+#     "32SC1":   (np.int32,   1),
+#     "32SC2":   (np.int32,   2),
+#     "32SC3":   (np.int32,   3),
+#     "32SC4":   (np.int32,   4),
+#     "32FC1":   (np.float32, 1),
+#     "32FC2":   (np.float32, 2),
+#     "32FC3":   (np.float32, 3),
+#     "32FC4":   (np.float32, 4),
+#     "64FC1":   (np.float64, 1),
+#     "64FC2":   (np.float64, 2),
+#     "64FC3":   (np.float64, 3),
+#     "64FC4":   (np.float64, 4)
+# }
 
 
 # def image_to_numpy(msg):
@@ -123,14 +123,13 @@ name_to_dtypes = {
 
 #         count += 1
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Image collection for extrinsic calibration between fixed camera and manipulator')
-    parser.add_argument('--output_dir', default='.', help='rosbag image output dir')
-    parser.add_argument('--msg_type', default='color', type=str, help='choose to save color or depth')
-    args = parser.parse_args()
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description='Image collection for extrinsic calibration between fixed camera and manipulator')
+#     parser.add_argument('--output_dir', default='.', help='rosbag image output dir')
+#     parser.add_argument('--msg_type', default='color', type=str, help='choose to save color or depth')
+#     args = parser.parse_args()
     
-    main(args)
+#     main(args)
 
 
 import os
@@ -146,14 +145,17 @@ def main():
     """Extract a folder of images from a rosbag.
     """
     parser = argparse.ArgumentParser(description="Extract images from a ROS bag.")
-    parser.add_argument("bag_file", help="Input ROS bag.")
-    parser.add_argument("output_dir", help="Output directory.")
-    parser.add_argument("image_topic", help="Image topic.")
+    parser.add_argument("--bag_file", help="Input ROS bag.")
+    parser.add_argument("--output_dir", help="Output directory.")
+    parser.add_argument("--image_topic", help="Image topic.")
 
     args = parser.parse_args()
 
-    print "Extract images from %s on topic %s into %s" % (args.bag_file,
-                                                          args.image_topic, args.output_dir)
+    if not os.path.isdir(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    print("Extract images from %s on topic %s into %s" % (args.bag_file,
+                                                          args.image_topic, args.output_dir))
 
     bag = rosbag.Bag(args.bag_file, "r")
     bridge = CvBridge()
@@ -161,8 +163,8 @@ def main():
     for topic, msg, t in bag.read_messages(topics=[args.image_topic]):
         cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
-        cv2.imwrite(os.path.join(args.output_dir, "frame%06i.png" % count), cv_img)
-        print "Wrote image %i" % count
+        cv2.imwrite(os.path.join(args.output_dir, "frame%06i.png"%count), cv_img)
+        print("Wrote image %i" % count)
 
         count += 1
 
